bac54ba9a84da726a394d9cf474e69203f9482be
Loo <lannsnolo@gmail.com>
2025-07-09 13:05:18 +0545

    Update vars.py

--- AdityaHalder/modules/vars.py
+++ AdityaHalder/modules/vars.py
@@ -11,17 +11,17 @@ if os.path.exists("Internal"):
 
 class Config(object):
     # REQUIRED VARIABLES
-    API_ID = int(getenv("API_ID", 0))
-    API_HASH = getenv("API_HASH", None)
-    BOT_TOKEN = getenv("BOT_TOKEN", None)
-    STRING_SESSION = getenv("STRING_SESSION", None)
-    MONGO_DATABASE = getenv("MONGO_DATABASE", None)
+    API_ID = int(getenv("API_ID", 20098819))
+    API_HASH = getenv("API_HASH", "2545d49cea8894d513726649b1bd5a1f")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7529913491:AAG43T71V7p6-cqFDRAlItKhTzucd1dUcws")
+    STRING_SESSION = getenv("STRING_SESSION", "BQEyrwMAscGp1Qb6RcS_8wVRPZhRMOhqW4pg_GfO4AkrToQDjBgN1v6Y_pO2Su8fo25oOPgoW5gOKtlAzjwavVtab5ticg0Y645AS7LVeN50GkMXPjNq6UAQLmAsfwNCPKI0570z1bS2ZhtqES2sM72SJPxdpsD8zYjYFd588nSAwOm-5f0HDiXKli885rsh6gPPEQ2JyakWjZQyPu29ZZtwDpmvGiI58hkNd1Vta4urRgxjVNuYsWqQPDFFm5anG3C51BBGJikxx8paHPlepM0ZbW7JUHJwvi2CmEhkx9otzt7GXQo16uUOIxOQPBFtDe98RL-X5ecoqVtBKTw6p3puTtwCUgAAAAHXQT_PAA")
+    MONGO_DATABASE = getenv("MONGO_DATABASE", "mongodb+srv://Alisha:Alisha123@cluster0.yqcpftw.mongodb.net/?retryWrites=true&w=majority")
   
     # OPTIONAL VARIABLES
     SESSION_STRING = getenv("SESSION_STRING", None)
     COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". ! > *").split())
     USERBOT_PICTURE = getenv("USERBOT_PICTURE", None)
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002435242326))
   
   
     # do not edit these variables
