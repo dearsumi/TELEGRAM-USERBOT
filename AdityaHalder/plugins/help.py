51774851ba5f30cc09cd63b2c32b9d5f5cabc642
Loo <lannsnolo@gmail.com>
2025-07-09 13:29:46 +0545

    Update help.py

--- AdityaHalder/plugins/help.py
+++ AdityaHalder/plugins/help.py
@@ -60,7 +60,7 @@ Genius Userbot » {__version__} ✨...
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [Aditya Server](https://t.me/AdityaServer).**"""
+🌷Powered By : [SUMI](https://t.me/dear_sumi).**"""
     if plug_match:
         plugin = plug_match.group(1)
         text = (
