bf3984faaff470ccc290a3b192d4021fa6bd009f
Loo <lannsnolo@gmail.com>
2025-07-09 13:28:04 +0545

    Update utils.py

--- AdityaHalder/modules/utils.py
+++ AdityaHalder/modules/utils.py
@@ -8,12 +8,13 @@ from typing import Union, List
 from youtubesearchpython import VideosSearch
 
 
-
 PREFIXES = Config.COMMAND_PREFIXES
 HANDLERS = Config.COMMAND_HANDLERS
 
+
 def commandx(commands: Union[str, List[str]]):
     return filters.command(commands, PREFIXES)
+
 
 def commandz(commands: Union[str, List[str]]):
     return filters.command(commands, HANDLERS)
@@ -30,22 +31,6 @@ def get_youtube_video(query: str):
         return 0
 
 
-async def get_youtube_stream(link: str):
-    proc = await asyncio.create_subprocess_exec(
-        'yt-dlp',
-        '-g',
-        '-f',
-        # CHANGE THIS BASED ON WHAT YOU WANT
-        'best[height<=?720][width<=?1280]',
-        link,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await proc.communicate()
-    return stdout.decode().split('\n')[0]
-
-
-
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
         return self.text == other.text
@@ -58,7 +43,7 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
 
 
 def paginate_plugins(page_n, plugin_dict, prefix, chat=None):
-    from ..import app
+    from .. import app
     if not chat:
         plugins = sorted(
             [
@@ -104,10 +89,9 @@ def paginate_plugins(page_n, plugin_dict, prefix, chat=None):
     max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
 
-    # can only have a certain amount of buttons side by side
     if len(pairs) > COLUMN_SIZE:
         pairs = pairs[
-            modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)
+            modulo_page * COLUMN_SIZE: COLUMN_SIZE * (modulo_page + 1)
         ] + [
             (
                 EqInlineKeyboardButton(
@@ -126,5 +110,3 @@ def paginate_plugins(page_n, plugin_dict, prefix, chat=None):
         ]
 
     return pairs
-
-    
